6ddf164b55b792c7098430ad18cfe406b791a17c
michaeladamstrickland <strickland1514@gmail.com>
2019-08-14 19:48:31 -0600

    new react port

---
+++ src/resumeData.js
@@ -0,0 +1,103 @@
+let resumeData = {
+    "name": "Michael Strickland",
+    "role": "Full Stack Developer and Software Engineer",
+    "linkedinId":"https://www.linkedin.com/in/michaeladamstrickland/",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I love the challenges and day to day learning that comes with building a great project.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/michaeladamstrickland//",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/michaeladamstrickland",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"At the very first time I wrote any code I knew this was the right carrer path for me, and I just couldn't wait to learn more. The day to day chellenges that come with development keep me always wanting more. Working with technology like JavaScript, MySQL, NodeJs, And React have been such an amazing and eye opening experience. I have been able to build some amazing things, and can not wait to see what I get to work on next. I work as a Full Stack Web Developer with a true passion for both front-end and back-end development. I love the art that comes with front-end, and the percision and challenges that come with the back-end.",
+    "address":" cell: (856)236-9293",
+    "website":"email: strickland1514@gmail.com",
+    "education":[
+      {
+        "UniversityName":"The University of Denver",
+        "specialization":"Full Stack Web Development",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2019",
+        "Achievements":"Certificate of Fucll Stack Web Development"
+      },
+     
+    ],
+    "work":[
+      {
+        "CompanyName":"Jay Ablah Photography",
+        "specialization":"Website Developer",
+        "MonthOfLeaving":"July",
+        "YearOfLeaving":"2019",
+        "Achievements":"Some Achievements"
+      },
+    
+    ],
+    "skillsDescription":"HTML, CSS, JAVASCRIPT, NODEJS, SQL, NOSQL, REACTJS",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"javascript"
+      },
+      {
+        "skillname":"reactjs"
+      },
+      {
+        "skillname":"sql"
+      },
+      {
+        "skillname":"nodejs"
+      }
+      
+    ],
+    "portfolio":[
+      {
+        "name":"Rock Paper Scissors",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/rockpaperscissors.jpg"
+      },
+      {
+        "name":"hangman",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/Hangman.jpg"
+      },
+      {
+        "name":"The Office Fan Page",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/theOffice.jpg"
+      },
+      {
+        "name":"Beach Resort",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/beachResort.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"If you're already a front-end developer, well, pretend you're also wearing a pirate hat",
+        "name":"Ethan Marcotte,"
+      },
+      {
+        "description":"Good design is like a refrigerator—when it works, no one notices, but when it doesn’t, it sure stinks.",
+        "name":"Irene Au"
+      }
+    ]
+  }
+  
+  export default resumeData
